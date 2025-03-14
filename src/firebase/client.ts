04bd1da82372f47db5fc2a4dcc6d1e18710527b1
Elijah Sean Galang <elijahseangalang@gmail.com>
2025-03-15 01:31:29 +0800

    Initial Commit

---
+++ src/firebase/client.ts
@@ -0,0 +1,31 @@
+import { initializeApp, getApps } from "firebase/app";
+import { Auth, getAuth } from "firebase/auth";
+import { FirebaseStorage, getStorage } from "firebase/storage";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+
+
+// Initialize Firebase
+const currentApps = getApps();
+let auth: Auth;
+let storage: FirebaseStorage;
+
+if (!currentApps.length) {
+  const app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+  storage = getStorage(app);
+} else {
+  const app = currentApps[0];
+  auth = getAuth(app);
+  storage = getStorage(app);
+}
+
+export { auth, storage };
---
+++ src/firebase/server.ts
@@ -0,0 +1,36 @@
+import { Firestore, getFirestore } from "firebase-admin/firestore";
+import { getApps, ServiceAccount } from "firebase-admin/app";
+import admin from "firebase-admin";
+import { Auth, getAuth } from "firebase-admin/auth";
+
+const serviceAccount = {
+  type:  process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_x509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_x509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
+
+let firestore: Firestore;
+let auth: Auth;
+const currentApps = getApps();
+
+if (!currentApps.length) {
+  const app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  });
+  firestore = getFirestore(app);
+  auth = getAuth(app);
+} else {
+  const app = currentApps[0];
+  firestore = getFirestore(app);
+  auth = getAuth(app);
+}
+
+export { firestore, auth };
